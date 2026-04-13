35fbc8bcf96a96fa55a0c9b7255bef75b755c2fd
benhurleykallidus <ben.hurley@kallidus.com>
2026-04-13 17:06:46 +0100

    added simple .net console program that tells you various details about the date and time (#3)
    * added simple .net console program that tells you various details about the date and time
    
    * added user input normalisation to avoid issues with capitalisation or whitespace

---
+++ datetime-details/Program.cs
@@ -0,0 +1,22 @@
+﻿var currentDateTime = DateTime.Now;
+var currentDay = DateTime.Now.DayOfWeek;
+var currentMonth = DateTime.Now.Month;
+var currentYear = DateTime.Now.Year;
+
+Console.WriteLine("Do you want the 1.DateTime, 2.Day, 3.Month, or 4.Year? Enter 1-4");
+
+var userInput = Console.ReadLine()?.Trim().ToLower();
+
+var message = userInput switch
+{
+	"1" => $"Current Date and Time: {currentDateTime}",
+	"2" => $"Current Day: {currentDay}",
+	"3" => $"Current Month: {currentMonth}",
+	"4" => $"Current Year: {currentYear}",
+	_ => "Invalid input. Please enter a number between 1 and 4."
+};
+
+Console.WriteLine(message);
+
+Console.WriteLine("Press any key to close...");
+
